bf6aa7fd54a98f9e1dc2737fee9baa0693faa427
yusufhnf <flyme.yusuf@gmail.com>
2021-08-26 05:33:19 +0700

    add core data file

---
+++ dolandolen/ViewController/FavoritesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesViewController.swift
+//  dolandolen
+//
+//  Created by Yusuf Umar Hanafi on 26/08/21.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ dolandolen/ViewModel/FavoritesViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FavoritesViewModel.swift
+//  dolandolen
+//
+//  Created by Yusuf Umar Hanafi on 26/08/21.
+//
+
+import Foundation
+
+class FavoritesViewModel {
+    
+}
